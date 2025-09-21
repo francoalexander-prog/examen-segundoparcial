d78ce02b7f7049af7b70f9a01c24339e5c274390
Franco Alexander Medina Miranda <franco.medina@uab.edu.bo>
2025-09-20 22:10:16 -0400

    feat(ui): showcase template components on /ui

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
     { path: '', loadComponent: () => import('../app/pages/home/home').then(m => m.Home) },
     { path: 'about', loadComponent: () => import('../app/pages/about/about').then(m => m.About) },
     { path: 'contact', loadComponent: () => import('../app/pages/contact/contact').then(m => m.Contact) },
+    { path: 'ui', loadComponent: () => import('../app/app/ui/ui').then(m => m.Ui) },
     { path: '**', redirectTo: '' }
 ];
