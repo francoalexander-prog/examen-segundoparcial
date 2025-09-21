688cdd44fa880d8006b32ef80babbf621ea3f4c8
Franco Alexander Medina Miranda <franco.medina@uab.edu.bo>
2025-09-20 22:40:05 -0400

    feat(pwa): enable service worker and web manifest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig, 
+  providers: [
+    ...(appConfig.providers || []), 
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
+  ],
+}).catch((err) => console.error(err));
