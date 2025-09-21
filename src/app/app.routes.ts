6dbec35fe4dedbb83ecf1bcd97e9b7dbdc2281a1
Franco Alexander Medina Miranda <franco.medina@uab.edu.bo>
2025-09-20 21:51:08 -0400

    feat(home): integrate template layout into Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('../app/pages/home/home').then(m => m.Home) },
+    { path: 'about', loadComponent: () => import('../app/pages/about/about').then(m => m.About) },
+    { path: 'contact', loadComponent: () => import('../app/pages/contact/contact').then(m => m.Contact) },
+    { path: '**', redirectTo: '' }
+];
